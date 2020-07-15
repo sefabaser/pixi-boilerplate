12e149c6dd610a720b4fd85dbe8c3aef390fda97
sefabaser <sefabaser84@hotmail.com>
2020-07-15 19:09:44 +0200

    package migration

--- config/webpack.main.config.js
+++ config/webpack.main.config.js
@@ -1,41 +1,33 @@
-const webpack = require('webpack');
-const HtmlPlugin = require('html-webpack-plugin');
-const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const webpack = require("webpack");
+const HtmlPlugin = require("html-webpack-plugin");
 
-const { mainPath, target, isDev } = require('./env');
+const { mainPath, target, isDev } = require("./env");
 
 module.exports = {
   entry: {
-    main: `${mainPath}/index.ts`
+    main: `${mainPath}/index.ts`,
   },
   output: {
     path: target,
-    filename: '[name].js',
-    publicPath: '/',
-    devtoolModuleFilenameTemplate: "file:///[absolute-resource-path]"
+    filename: "[name].js",
+    publicPath: "/",
+    devtoolModuleFilenameTemplate: "file:///[absolute-resource-path]",
   },
   node: {
-    __dirname: false
+    __dirname: false,
   },
   // Enable sourcemaps for debugging webpack's output.
-  devtool: isDev && 'inline-source-map',
+  devtool: isDev && "inline-source-map",
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".js"],
-    plugins: [
-      new TsConfigPathsPlugin()
-    ]
   },
-  plugins: [
-    new CheckerPlugin()
-  ],
   module: {
     rules: [
-      // All files with a '.ts' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.ts$/, use: ["awesome-typescript-loader"] },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, enforce: "pre", use: "source-map-loader" }
-    ]
+      { test: /\.js$/, enforce: "pre", use: "source-map-loader" },
+    ],
   },
-  target: 'electron'
+  target: "electron",
 };
--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -1,54 +1,55 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
-const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader')
+const webpack = require("webpack");
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 
-const { dependencies, rendererPath, tsconfig, template, target, isDev } = require('./env');
+const {
+  dependencies,
+  rendererPath,
+  tsconfig,
+  template,
+  target,
+  isDev,
+} = require("./env");
 
 module.exports = {
   entry: {
     renderer: `${rendererPath}/index.ts`,
-    vendor: Object.keys(dependencies)
+    vendor: Object.keys(dependencies),
   },
   output: {
     path: target,
-    filename: '[name].js',
+    filename: "[name].js",
   },
 
   devServer: {
-    port: 3000
+    port: 3000,
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: isDev && 'inline-source-map',
+  devtool: isDev && "inline-source-map",
   resolve: {
     extensions: [".ts", ".js"],
-    plugins: [
-      new TsConfigPathsPlugin()
-    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlPlugin({
       title: "Electron",
-      filename: 'index.html',
-      template
+      filename: "index.html",
+      template,
     }),
-    new CheckerPlugin(),
-    new webpack.optimize.CommonsChunkPlugin('vendor'),
+    new webpack.optimize.CommonsChunkPlugin("vendor"),
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    })
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ],
   module: {
     rules: [
-      // All files with a '.ts' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.ts$/, use: ["awesome-typescript-loader"] },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, enforce: "pre", use: "source-map-loader" }
-    ]
+      { test: /\.js$/, enforce: "pre", use: "source-map-loader" },
+    ],
   },
-  target: 'electron-renderer'
+  target: "electron-renderer",
 };
