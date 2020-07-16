18d13caa4ad2673dfa987b82b902af4549a8f218
sefabaser <sefabaser84@hotmail.com>
2020-07-17 01:50:26 +0200

    browser window styling

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,24 @@ import { app, BrowserWindow } from 'electron';
 const isDev = process.env.NODE_ENV === 'development';
 
 app.on('ready', () => {
-  const win = new BrowserWindow({
+  let options: Electron.BrowserWindowConstructorOptions = {
     width: 1238,
     height: 1000,
     webPreferences: { nodeIntegration: true }
-  });
+    // backgroundColor: '#000000'
+  };
 
-  const url = isDev ? 'http://localhost:3000' : `file://${__dirname}/index.html`;
+  if (!isDev) {
+    options.resizable = false;
+  }
 
-  win.loadURL(url);
-  isDev && win.webContents.openDevTools();
+  let win = new BrowserWindow(options);
+  if (!isDev) {
+    // tslint:disable-next-line: no-null-keyword
+    win.setMenu(null);
+  } else {
+    win.webContents.openDevTools();
+  }
+
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${__dirname}/index.html`);
 });
