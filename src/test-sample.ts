4b53f1e0d36b9385779cbe7cb88a24f8e45cd0bd
sefabaser <sefabaser84@hotmail.com>
2020-07-16 21:33:00 +0200

    unit test setup

--- src/test-sample.ts
+++
@@ -1,4 +0,0 @@
-export function multiply(a: number, b: number) {
-  // throw new Error('error');
-  return a * b;
-}
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,13 +1,5 @@
-import { multiply } from './test-sample';
-
 describe('Sample', () => {
   it('should success', () => {
     expect(true).toBe(true);
-  });
-});
-
-describe('When multiplying numbers', () => {
-  it('Should be able to compute result', () => {
-    expect(multiply(2, 3)).toBe(6);
   });
 });
